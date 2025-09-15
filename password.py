14741d408577a6a10080039ce0e351176c5f65fd
chandanasiri754-cyber <chandanasiri754@gmail.com>
2025-09-15 15:56:51 +0530

    password.py

--- password.py
+++ password.py
@@ -1,2 +1,5 @@
 import getpass
 d={"Siri": "2006", "Tarini": "2009", "Hema": "1982", "Gautham": "1975"}
+username = input("Enter your username: ")
+if username in d:
+    password = getpass.getpass("Enter your password: ")
